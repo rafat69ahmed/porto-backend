5bbbb574bd668709816bf7d07243d81bdff3e47c
rafat69ahmed <rafat69ahmed@gmail.com>
2024-07-06 11:46:01 +0600

    feat: authentication done

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::group(['prefix' => 'v1'], function () {
+
+    Route::controller(AuthController::class)->group(function () {
+        Route::post('login', 'login');
+        Route::post('register', 'register');
+        Route::post('logout', 'logout');
+    });
+});
